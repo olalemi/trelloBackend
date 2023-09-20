c014f3af31f5750e809ad4e7785e563f2bd3dbaa
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-09-21 00:17:25 +0100

    cors updated

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -7,7 +7,7 @@ module.exports = {
   createTask: async (req, res, next) => {
     try {
       const task = await Task.create({...req.body});
-      res.status(201).json({ msg: 'successfully added a task', task });
+     return res.status(201).json({ msg: 'successfully added a task', task });
     }catch (error) {
       return next(error);
   }  
@@ -17,7 +17,7 @@ module.exports = {
   getAllTasks: async (req, res, next) => {
     try {
       const tasks = await Task.find({});
-      res.status(200).json({ tasks, number: tasks.length });
+     return res.status(200).json({ tasks, number: tasks.length });
     } catch (error) {
       return next(error);
   }  
@@ -31,7 +31,7 @@ module.exports = {
       const taskId = req.params.id;
       const task = await Task.findById(taskId);
       //check if task Id is valid
-      if (!task || ObjectId.isValid(taskId) == false) {
+      if (!task ) {
       return res.status(404).json({
              message: `No task with id: ${taskId}` 
      });
@@ -50,11 +50,11 @@ module.exports = {
       const taskId = req.params.id;
       const task = await Task.findByIdAndUpdate(taskId, req.body);
       //check if Id is valid
-      if (!task || ObjectId.isValid(taskId) == false) {
-      return res.status(404).json({ 
-             message: `No task with id: ${taskId}` 
-     });
-   } 
+      if (!task) {
+        return res.status(404).json({ 
+          message: `No task with id: ${taskId}` 
+        });
+      }
       res.status(200).json({ message: `task successfuly updated` });
     } catch (error) {
       return next(error);
@@ -63,16 +63,18 @@ module.exports = {
   },
 
 
+
   //Delete task by Id
   deleteTaskById: async (req, res, next) => {
     try {
       const taskId = req.params.id;
       const task = await Task.findByIdAndDelete(taskId);
       //check if Id is a valid
-      if (!task || ObjectId.isValid(taskId) == false) {
-     return res.status(404).json({
-            message: `No task with id: ${taskId}` });
-    }
+      if (!task) {
+        return res.status(404).json({ 
+          message: `No task with id: ${taskId}` 
+        });
+      }
       res.status(200).json({message: `task successfuly deleted`});
     } catch (error) {
       return next(error);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const connectDatabase = require('./database/connect_DB');
 const route = require("./routes/task.route")
 const { notFound } = require('./middlewares/errors/notFound');
 const errorHandler = require('./middlewares/errors/errorHandler');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -18,6 +19,7 @@ app.use('/api/v1', route);
 
 app.use(errorHandler);
 app.use(notFound);
+app.use(cors());
  
 connectDatabase(async () => {
     app.listen(PORT, () => {
